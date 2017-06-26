d0a39034b98ebf9f30e5351392cacbe622471ea6
Edward Herbert <eherbert@bgea.org>
2017-06-25 21:33:41 -0400

    removed test file

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		 <script src="https://use.fontawesome.com/7fc0a020a0.js"></script>
-     <link href="/wp-content/themes/abeherbert/css/main.css" rel="stylesheet">
+     <link href="/wp-content/themes/abemeister/css/main.css" rel="stylesheet">
    </head>
  <body>
 	 <header class="fullWindow">
