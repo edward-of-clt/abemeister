8c80ff042cc69c2bdb7f67a22fc8c2cc429aa5ea
Edward Herbert <abeh78@gmail.com>
2017-11-12 13:41:59 -0500

    Update and rename resume.php to general_page.php

--- general_page.php
+++ general_page.php
@@ -1,5 +1,5 @@
 <?php
-// Template Name: Resume Page
+// Template Name: General Page
 get_header(); ?>
 
 <?php while ( have_posts() ) { the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
